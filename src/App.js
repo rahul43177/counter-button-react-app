f09301e5a1d4ce71187d8fd5403b78a77bc421bb
Rahul <rahulmishra4317@gmail.com>
2023-07-18 12:04:16 +0530

    button counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import {useState} from 'react'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter , setCounter] = useState(0)
+
+    const handleNegative = () => {
+        setCounter(counter-1)
+    }
+
+    const handlePositive = () =>{
+        setCounter(counter+1)
+    }
+    return (
+        <div className='button'>
+            <button onClick={handleNegative}>-</button>
+            <h1>{counter}</h1>
+            <button onClick={handlePositive}>+</button>
+        </div>
+    )
 }
 
-export default App;
+
+export default App
